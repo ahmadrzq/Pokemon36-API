3f567cec37a296b7107c1c5b23e533fdd392e4c8
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-03-22 13:03:51 +0700

    Update script to change color of card wrapper base on type of pokemon

--- script.js
+++ script.js
@@ -15,18 +15,83 @@ async function getData() {
         data +
         `<div class="card">
             <div class="card-wrapper">
-                <p><span>${detailPokemon.id}</span> : <span>${
-          detailPokemon.name
-        }</span></p>
-                <img src="${detailPokemon.sprites.front_default}" alt="${
-          detailPokemon.name
-        }" />
-                <p>Type : <span>${detailPokemon.types[0].type.name}</span></p>
+                <p><span>${detailPokemon.id}</span> : <span>${detailPokemon.name}</span></p>
+                <img src="${detailPokemon.sprites.front_default}" alt="${detailPokemon.name}" />
+                <p>Type : <span class="type">${detailPokemon.types[0].type.name}</span></p>
             </div>
         </div>`;
     }
 
     document.getElementById("data-pokemon").innerHTML = data;
+
+    // Get value type of pokemon
+    const typePokemon = document.querySelectorAll(".type");
+
+    // Change the bg color based on type of pokemon
+    for (let i = 0; i < typePokemon.length; i++) {
+      // Get bgcolor of card.wrapper
+      const colorCard = document.querySelectorAll(".card-wrapper");
+    //   if (typePokemon[i].innerHTML === "grass") {
+    //   }
+      switch (typePokemon[i].innerHTML) {
+        case "grass":
+          colorCard[i].classList.add("bg-grass");
+          break;
+        case "water":
+          colorCard[i].classList.add("bg-water");
+          break;
+        case "fire":
+          colorCard[i].classList.add("bg-fire");
+          break;
+        case "bug":
+          colorCard[i].classList.add("bg-bug");
+          break;
+        case "normal":
+          colorCard[i].classList.add("bg-normal");
+          break;
+        case "flying":
+          colorCard[i].classList.add("bg-flying");
+          break;
+        case "rock":
+          colorCard[i].classList.add("bg-rock");
+          break;
+        case "ground":
+          colorCard[i].classList.add("bg-ground");
+          break;
+        case "psychic":
+          colorCard[i].classList.add("bg-psychic");
+          break;
+        case "ghost":
+          colorCard[i].classList.add("bg-ghost");
+          break;
+        case "dark":
+          colorCard[i].classList.add("bg-dark");
+          break;
+        case "steel":
+          colorCard[i].classList.add("bg-steel");
+          break;
+        case "poison":
+          colorCard[i].classList.add("bg-poison");
+          break;
+        case "electric":
+          colorCard[i].classList.add("bg-electric");
+          break;
+        case "fairy":
+          colorCard[i].classList.add("bg-fairy");
+          break;
+        case "fighting":
+          colorCard[i].classList.add("bg-fighting");
+          break;
+        case "dragon":
+          colorCard[i].classList.add("bg-dragon");
+          break;
+        case "ice":
+          colorCard[i].classList.add("bg-ice");
+          break;
+        default:
+          break;
+      }
+    }
   } catch (error) {
     console.log(`Error Message : ${error}`);
   }
@@ -35,3 +100,4 @@ async function getData() {
 function resetCards() {
   document.querySelector("#data-pokemon").innerHTML = "";
 }
+
